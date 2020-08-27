6f3934edf6fc8d965a9bf92966d2f350b0a57481
univac <68467947+univac-os@users.noreply.github.com>
2020-08-27 15:30:34 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,17 +1,28 @@
 #include <assert.h>
 
+const int bpmLimit={70,150};
+const int resRateLimit={30,95};
+
+bool bpmCheck(float bpm){
+  return  (bpm>bpmLimit[0] && bpm<bpmLimit[1]);
+}
+bool resRateCheck(float resRate){
+  return  (bpm>resRateLimit[0] && bpm<resRateLimit[1]);
+}   
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
+  if(!bpmCheck) {
     return false;
   } else if(spo2 < 90) {
     return false;
-  } else if(respRate < 30 || respRate > 95) {
+  } else if(!respRateCheck) {
     return false;
   }
   return true;
 }
 
 int main() {
-  assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);
+  assert(vitalsAreOk(80, 95, 60) == true);  // 1 1  1
+  assert(vitalsAreOk(60, 90, 40) == false); // 0 1  1
+  assert(vitalsAreOk(90,85,70)==false);// 1 0 1
+  assert(vitalsAreOk(100,95,100)==false);// 1 1 0
 }
