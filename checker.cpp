7d1dcf8661697da9a17b82d7c0ceed85709c2f23
univac <68467947+univac-os@users.noreply.github.com>
2020-08-27 17:43:45 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -12,17 +12,22 @@ bool spo2Check(float spo2){
 bool respRateCheck(float respRate){
   return  (respRate>respRateLimit[0] && respRate<respRateLimit[1]);
 }
-*/
+*/ //here we made one fun for everything
 bool vitalIsOk(float value, const float *limits){
-  return (value >= limits[0] && value <= limits[1]);
+ // return (value >= limits[0] && value <= limits[1]);
+  if(value<limits[0]){cout<<"below"<<endl;return false;}
+  else if(value>limits[0]){cout<<"above"<<endl;return false;}
+  else {cout<<"all okay"<<endl;return true;}
  }
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
   /*if( int(bpmCheck(bpm))+ int(spo2Check(spo2)) + int(respRateCheck(respRate))!=3){return false;}
   else return true;
   return (bpmCheck(bpm) && spo2Check(spo2) && respRateCheck(respRate);
 */
+  int bool_array[]={int(vitalIsOk(bpm,bpmLimit)),int(vitalIsOk(spo2,spo2Limit)),int(vitalIsOk(respRate,respRateLimit))};
   if(int(vitalIsOk(bpm,bpmLimit)) +int( vitalIsOk(spo2,spo2Limit)) +int(vitalIsOk(respRate,respRateLimit))!=3){return false;}
   else return true;
+    
 }//CCN-2
 
 int main() {
