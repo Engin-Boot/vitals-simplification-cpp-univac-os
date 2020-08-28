25e8125274151880e869db7d3276607add70159b
univac <68467947+univac-os@users.noreply.github.com>
2020-08-28 16:09:05 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -4,39 +4,63 @@ using namespace std;
 const float bpmLimit[]={70,150};
 const float spo2Limit[]={90,100};//make INT_MAX
 const float respRateLimit[]={30,95};
-/*bool bpmCheck(float bpm){
-  return  (bpm>bpmLimit[0] && bpm<bpmLimit[1]);
+class Alert
+{
+  public:
+    virtual void sendAlert(const char*,const char*)=0;//pure virtual fun
+};
+class AlertSms:public Alert
+{
+  public:
+    void sendAlert(const char* vitalName,const char* value){
+      cout<<"SMS:"<<vitalName<<"--------"<<value<<endl;
 }
-bool spo2Check(float spo2){
-  return  (spo2>spo2Limit);
+class AlertSound:public Alert
+{
+  public:
+    void sendAlert(const char* vitalName,const char* value){
+      cout<<"SOUND:"<<vitalName<<"--------"<<value<<endl;
 }
 
-bool respRateCheck(float respRate){
-  return  (respRate>respRateLimit[0] && respRate<respRateLimit[1]);
-}
-*/ //here we made one fun for everything
-bool vitalIsOk(float value, const float *limits){
- // return (value >= limits[0] && value <= limits[1]);
-  if(value<limits[0]){cout<<"below"<<endl;return false;}
-  else if(value>limits[1]){cout<<"above"<<endl;return false;}
-  else {cout<<"all okay"<<endl;return true;}
+class vitals{
+ public:
+    void vitalIsOk(float value, const float *limits){
+      if(value<limits[0]){cout<<"below"<<endl;return false;}
+      else if(value>limits[1]){cout<<"above"<<endl;return false;}
+      else {cout<<"all okay"<<endl;return true;}
  }
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  /*if( int(bpmCheck(bpm))+ int(spo2Check(spo2)) + int(respRateCheck(respRate))!=3){return false;}
-  else return true;
-  return (bpmCheck(bpm) && spo2Check(spo2) && respRateCheck(respRate);
-*/
-  int bool_array[]={int(vitalIsOk(bpm,bpmLimit)),int(vitalIsOk(spo2,spo2Limit)),int(vitalIsOk(respRate,respRateLimit))};
-  if(int(vitalIsOk(bpm,bpmLimit)) +int( vitalIsOk(spo2,spo2Limit)) +int(vitalIsOk(respRate,respRateLimit))!=3){return false;}
-  else return true;
-  
-}//CCN-2
-
+    void vitalsAreOk(float bpm, float spo2, float respRate) {
+       // int bool_array[]={int(vitalIsOk(bpm,bpmLimit)),int(vitalIsOk(spo2,spo2Limit)),int(vitalIsOk(respRate,respRateLimit))};
+        //if(int(vitalIsOk(bpm,bpmLimit)) +int( vitalIsOk(spo2,spo2Limit)) +int(vitalIsOk(respRate,respRateLimit))!=3){return false;}
+        //else return true;
+        const char* bpmlevel=vitalsIsOk(bpm,bpmLimit);
+        const char* spo2level=vitalsIsOk(spo2,spo2Limit);
+        const char* respRatelevel=vitalsIsOk(respRate,respRateLimit);
+        //alerting
+        Alert *alert;
+        AlertSms alertSms;
+        AlertSound alertSound;
+        //derv class addr pass to bas class pointer
+        alert=&alertSms
+        alert->sendAlert("BPM",bpmLevel);
+        alert->sendAlert("SPO2",spo2Level);
+        alert->sendAlert("RESPRATE",resRateLevel);
+        
+        alert=&alertSound
+        alert->sendAlert("BPM",bpmLevel);
+        alert->sendAlert("SPO2",spo2Level);
+        alert->sendAlert("RESPRATE",resRateLevel);
+        
+    }//CCN-2
+};
 int main() {
-  assert(vitalsAreOk(80, 95, 60) == true);  // 1 1  1
+  /*assert(vitalsAreOk(80, 95, 60) == true);  // 1 1  1
   assert(vitalsAreOk(60, 90, 40) == false); // 0 1  1
   assert(vitalsAreOk(90,85,70)==false);// 1 0 1
   assert(vitalsAreOk(100,95,100)==false);// 1 1 0
-  assert(vitalsAreOk(50,85,99)==false);//0  0 0
+  assert(vitalsAreOk(50,85,99)==false);//0  0 0 */
+  vitals vital;
+  vital.vitalsAreOk(80,95,60);
+  vital.vitalsAreOk(60, 90, 40);
 
 }
